d0b0c633698d32bf9110e8614dc9666e546cf126
Marián Svitek <marian.svitek99@gmail.com>
2025-07-23 11:21:32 +0200

    Added validation translations for infopoint and sequence schemas

--- web/src/components/dialogs/infopoint-dialog/schema.ts
+++ web/src/components/dialogs/infopoint-dialog/schema.ts
@@ -1,61 +1,61 @@
 import * as Yup from "yup";
 import { InfopointBodyType, InfopointShape } from "models/infopoint";
+import { TFunction } from "i18next";
 
-export const infopointSchema = Yup.object({
-  header: Yup.string().optional(),
-  bodyContentType: Yup.mixed<InfopointBodyType>().oneOf([
-    "TEXT",
-    "IMAGE",
-    "VIDEO",
-  ]),
-  text: Yup.string().when("bodyContentType", {
-    is: (bodyContentType: InfopointBodyType) => bodyContentType === "TEXT",
-    then: (schema) =>
-      schema.optional().max(150, "*Maximálně 150 znaků."),
-  }),
-  imageName: Yup.string().when("bodyContentType", {
-    is: (bodyContentType: InfopointBodyType) => bodyContentType === "IMAGE",
-    then: (schema) => schema.required("*Povinné"),
-  }),
-  // imageFile is display none
-  videoName: Yup.string().when("bodyContentType", {
-    is: (bodyContentType: InfopointBodyType) => bodyContentType === "VIDEO",
-    then: (schema) => schema.required("*Povinné"),
-  }),
-  // videoFile is display none
-  alwaysVisible: Yup.boolean().optional(),
+export const retrieveInfopointSchema = (t: TFunction) => {
+  return Yup.object({
+    header: Yup.string().optional(),
+    bodyContentType: Yup.mixed<InfopointBodyType>().oneOf([
+      "TEXT",
+      "IMAGE",
+      "VIDEO",
+    ]),
+    text: Yup.string().when("bodyContentType", {
+      is: (bodyContentType: InfopointBodyType) => bodyContentType === "TEXT",
+      then: (schema) => schema.optional().max(150, t("max150Chars")),
+    }),
+    imageName: Yup.string().when("bodyContentType", {
+      is: (bodyContentType: InfopointBodyType) => bodyContentType === "IMAGE",
+      then: (schema) => schema.required(t("required")),
+    }),
+    // imageFile is display none
+    videoName: Yup.string().when("bodyContentType", {
+      is: (bodyContentType: InfopointBodyType) => bodyContentType === "VIDEO",
+      then: (schema) => schema.required(t("required")),
+    }),
+    // videoFile is display none
+    alwaysVisible: Yup.boolean().optional(),
 
-  isUrlIncluded: Yup.boolean().optional(),
-  url: Yup.string().when("isUrlIncluded", {
-    is: (isUrlIncluded: boolean) => isUrlIncluded === true,
-    then: (schema) => schema.required("*Povinné"),
-  }),
-  urlName: Yup.string().when("isUrlIncluded", {
-    is: (isUrlIncluded: boolean) => isUrlIncluded === true,
-    then: (schema) => schema.required("*Povinné"),
-  }),
+    isUrlIncluded: Yup.boolean().optional(),
+    url: Yup.string().when("isUrlIncluded", {
+      is: (isUrlIncluded: boolean) => isUrlIncluded === true,
+      then: (schema) => schema.required(t("required")),
+    }),
+    urlName: Yup.string().when("isUrlIncluded", {
+      is: (isUrlIncluded: boolean) => isUrlIncluded === true,
+      then: (schema) => schema.required(t("required")),
+    }),
 
-  isScreenIdIncluded: Yup.boolean().optional(),
-  screenIdReference: Yup.string().when("isScreenIdIncluded", {
-    is: (isScreenIdIncluded: boolean) => isScreenIdIncluded === true,
-    then: (schema) => schema.required("*Povinné"),
-  }),
-  screenNameReference: Yup.string().when("isScreenIdIncluded", {
-    is: (isScreenIdIncluded: boolean) => isScreenIdIncluded === true,
-    then: (schema) => schema.required("*Povinné"),
-  }),
+    isScreenIdIncluded: Yup.boolean().optional(),
+    screenIdReference: Yup.string().when("isScreenIdIncluded", {
+      is: (isScreenIdIncluded: boolean) => isScreenIdIncluded === true,
+      then: (schema) => schema.required(t("required")),
+    }),
+    screenNameReference: Yup.string().when("isScreenIdIncluded", {
+      is: (isScreenIdIncluded: boolean) => isScreenIdIncluded === true,
+      then: (schema) => schema.required(t("required")),
+    }),
 
-  shape: Yup.mixed<InfopointShape>().oneOf(["SQUARE", "CIRCLE", "ICON"]),
-  pxSize: Yup.number()
-    .min(1, "Nemůže být menší než 1 pixel")
-    .required("*Povinné"),
-  color: Yup.string().when("shape", {
-    is: (shape: InfopointShape) => shape !== "ICON",
-    then: (schema) => schema.required("*Povinné"),
-  }),
-  iconName: Yup.string().when("shape", {
-    is: (shape: InfopointShape) => shape === "ICON",
-    then: (schema) => schema.required("*Povinné"),
-  }),
-  // iconFile is display none
-});
+    shape: Yup.mixed<InfopointShape>().oneOf(["SQUARE", "CIRCLE", "ICON"]),
+    pxSize: Yup.number().min(1, t("atLeastOnePixel")).required(t("required")),
+    color: Yup.string().when("shape", {
+      is: (shape: InfopointShape) => shape !== "ICON",
+      then: (schema) => schema.required(t("required")),
+    }),
+    iconName: Yup.string().when("shape", {
+      is: (shape: InfopointShape) => shape === "ICON",
+      then: (schema) => schema.required(t("required")),
+    }),
+    // iconFile is display none
+  });
+};
--- web/src/components/dialogs/sequence-dialog/SequenceDialogNew.tsx
+++ web/src/components/dialogs/sequence-dialog/SequenceDialogNew.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { Formik } from "formik";
 import DialogWrap from "../dialog-wrap-noredux-typed";
 
 import SequenceForm from "./SequenceForm";
-import { sequenceSchema } from "./schema";
+import { retrieveSequenceSchema } from "./schema";
 import { SequenceFormData } from "./models";
 
 import { ZOOM_SCREEN_DEFAULT_STAY_IN_DETAIL_TIME } from "constants/screen";
@@ -22,7 +22,14 @@ const SequenceDialogNew = ({
   onDialogSubmit,
 }: SequenceDialogNewProps) => {
   const { t } = useTranslation("expo-editor", { keyPrefix: "sequenceForm" });
+  const { t: validationT } = useTranslation("validation");
+
   const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  const sequenceSchema = useMemo(
+    () => retrieveSequenceSchema(validationT),
+    [validationT]
+  );
 
   return (
     <Formik<SequenceFormData>
