283da6ff76c559675a32827b9b3b8de4d0c01ec6
Marián Svitek <marian.svitek99@gmail.com>
2025-07-23 12:41:26 +0200

    Added typescript typings to the validation file

--- web/src/components/form/redux-form/validation.js
+++
@@ -1,6 +0,0 @@
-export const required = (t) => (value) => value ? undefined : t("required");
-
-export const email = (t) => (value) =>
-  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? undefined
-    : t("invalidEmailFormat");
