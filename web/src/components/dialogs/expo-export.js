3aa3958700df41c928a5af2c48520832a6650545
Marián Svitek <marian.svitek99@gmail.com>
2025-07-23 10:24:14 +0200

    Added translations for Expo Export Dialog

--- web/src/components/dialogs/expo-export.js
+++ web/src/components/dialogs/expo-export.js
@@ -2,19 +2,24 @@ import { compose, withHandlers } from "recompose";
 import { reduxForm } from "redux-form";
 import { withRouter } from "react-router-dom";
 import Dialog from "./dialog-wrap";
+import { useTranslation } from "react-i18next";
 
-const ExpoExport = ({ handleSubmit }) => (
-  <Dialog
-    title="Export výstavy"
-    name="ExpoExport"
-    handleSubmit={handleSubmit}
-    submitLabel="Exportovat"
-  >
-    <form onSubmit={handleSubmit}>
-      <p>Export výstavy</p>
-    </form>
-  </Dialog>
-);
+const ExpoExport = ({ handleSubmit }) => {
+  const { t } = useTranslation("expo");
+
+  return (
+    <Dialog
+      title={t("expoExportDialog.title")}
+      name="ExpoExport"
+      handleSubmit={handleSubmit}
+      submitLabel={t("expoExportDialog.submitLabel")}
+    >
+      <form onSubmit={handleSubmit}>
+        <p>{t("expoExportDialog.content")}</p>
+      </form>
+    </Dialog>
+  );
+};
 
 export default compose(
   withRouter,
