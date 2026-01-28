16133440047ed5b64226355a8bdf332744a389cf
Tomas Martiska <tomas.martiska@inqool.cz>
2026-01-28 09:00:12 +0100

    added new endpoint

--- api/src/main/java/cz/inqool/uas/indihu/controller/SurveyController.java
+++ api/src/main/java/cz/inqool/uas/indihu/controller/SurveyController.java
@@ -1,6 +1,7 @@
 package cz.inqool.uas.indihu.controller;
 
 import cz.inqool.uas.indihu.entity.domain.SurveyAnswer;
+import cz.inqool.uas.indihu.entity.dto.SurveyAggregationDto;
 import cz.inqool.uas.indihu.entity.dto.SurveyAnswerDto;
 import cz.inqool.uas.indihu.service.SurveyService;
 import io.swagger.annotations.*;
@@ -49,6 +50,19 @@ public class SurveyController {
     public void deleteSurveyAnswers(@ApiParam(value = "Exposition ID", required = true) @PathVariable("expoId") String expoId,
                                     @ApiParam(value = "Screen ID", required = true) @PathVariable("screenId") String screenId) {
         surveyService.deleteSurveyAnswers(expoId, screenId);
+    }
+
+    @ApiOperation(value = "Gets aggregated survey statistics for a specific exposition and screen")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Aggregated survey data retrieved successfully", response = SurveyAggregationDto.class)
+    })
+    @RequestMapping(method = RequestMethod.GET, value = "/aggregate/{expoId}/{screenId}")
+    public SurveyAggregationDto getAggregatedSurveyData(
+            @ApiParam(value = "Exposition ID", required = true) @PathVariable("expoId") String expoId,
+            @ApiParam(value = "Screen ID", required = true) @PathVariable("screenId") String screenId,
+            @ApiParam(value = "Include free text answers in response", required = false)
+            @RequestParam(value = "includeFreeAnswers", required = false, defaultValue = "false") boolean includeFreeAnswers) {
+        return surveyService.getAggregatedSurveyData(expoId, screenId, includeFreeAnswers);
     }
 
     @Inject
