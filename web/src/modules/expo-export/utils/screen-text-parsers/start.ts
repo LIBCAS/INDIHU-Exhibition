30c5a9c5a44a00882781fb5327fd16cb7ff832d9
Marián Svitek <marian.svitek99@gmail.com>
2026-08-12 11:59:04 +0200

    fix: added some important fields to start screen export

--- web/src/modules/expo-export/utils/screen-text-parsers/start.ts
+++ web/src/modules/expo-export/utils/screen-text-parsers/start.ts
@@ -1,6 +1,6 @@
 import { StartScreen } from "models";
 import { TextSections } from "modules/expo-export/typings";
-import { addText } from "../common-text-parsers";
+import { addText, getPlainText } from "../common-text-parsers";
 
 export const parseStartScreen = (
   screen: StartScreen,
@@ -8,4 +8,22 @@ export const parseStartScreen = (
 ): void => {
   addText(sections, "Podnadpis", screen.subTitle);
   addText(sections, "Text obrazovky", screen.perex);
+  addText(sections, "Organizace", screen.organization);
+  addText(sections, "Webové stránky organizace", screen.organizationLink);
+
+  const imprint = screen.collaborators
+    ?.reduce<string[]>((entries, { role, text }) => {
+      const entry = [getPlainText(role), getPlainText(text)]
+        .filter(Boolean)
+        .join("\n");
+
+      if (entry) {
+        entries.push(entry);
+      }
+
+      return entries;
+    }, [])
+    .join("\n\n");
+
+  addText(sections, "Tiráž", imprint);
 };
